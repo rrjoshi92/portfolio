8cb91c68652fa891e5cbdf7dee30362b6f25f952
Ravi Joshi <ravi.r.joshi@kp.org>
2021-06-18 12:11:47 -0700

    disabling preloader and css minified.

--- js/custom.js
+++ js/custom.js
@@ -1,26 +1,26 @@
-/**	
+/**
 	* Template Name: MU Material
-	* Version: 1.0	
+	* Version: 1.0
 	* Template Scripts
 	* Author: MarkUps
 	* Author URI: http://www.markups.io/
 
 	Custom JS
-	
+
 	1. MOBILE MENU
 	2. EXPERIENCE SLIDER (Owl Carousel)
 	3. EDUCATION SLIDER (Owl Carousel)
 	4. PORTFOLIO SLIDER
 	5. COUNTER
-	6. TESTIMONIAL SLIDER (Owl Carousel)	
+	6. TESTIMONIAL SLIDER (Owl Carousel)
 	7. MENU SMOOTH SCROLLING
 	8. PRELOADER
 	9. CALL TO ABOUT
-	10. BOTTOM TO UP 
+	10. BOTTOM TO UP
 	11. PARALLAX HEADER
 	12. HIRE ME SCROLL
-	
-	
+
+
 **/
 
 jQuery(function($){
@@ -31,12 +31,12 @@ jQuery(function($){
 	/* ----------------------------------------------------------- */
 
     jQuery(".button-collapse").sideNav();
-    
+
 	/* ----------------------------------------------------------- */
 	/*  2. Experience SLider(Owl Carousel)
 	/* ----------------------------------------------------------- */
 
-	var owl = $("#owl-carousel"); 
+	var owl = $("#owl-carousel");
     owl.owlCarousel({
         items : 4, //4 items above 1024px browser width
         itemsDesktop : [1024,3], //3 items between 1024px and 901px
@@ -63,14 +63,14 @@ jQuery(function($){
         active: false,
         heightStyle: "content",
         icons: false
-    }); 
-   
+    });
+
 
     /* ----------------------------------------------------------- */
 	/*  3. EDUCATION SLIDER (Owl Carousel)
 	/* ----------------------------------------------------------- */
 
-	var owl1 = $("#owl-carousel1"); 
+	var owl1 = $("#owl-carousel1");
 	owl1.owlCarousel({
 	    items : 3, //3 items above 1024px browser width
 	    itemsDesktop : [1024,3], //3 items between 1024px and 901px
@@ -86,12 +86,12 @@ jQuery(function($){
 	jQuery(".prev1").click(function(){
 	    owl1.trigger('owl.prev');
 	});
-	
+
     /* ----------------------------------------------------------- */
 	/*  4. PORTFOLIO SLIDER
 	/* ----------------------------------------------------------- */
 
-	jQuery('#portfolio-list').mixItUp();	
+	jQuery('#portfolio-list').mixItUp();
 
 	/* ----------------------------------------------------------- */
 	/*  5. COUNTER
@@ -100,15 +100,15 @@ jQuery(function($){
 	jQuery('.counter').counterUp({
         delay: 10,
         time: 1000
-    });	  
+    });
 
 	/* ----------------------------------------------------------- */
 	/*  6. TESTIMONIAL SLIDER (Owl Carousel)
 	/* ----------------------------------------------------------- */
 
-	var owl2 = $("#owl-carousel2"); 
+	var owl2 = $("#owl-carousel2");
     owl2.owlCarousel({
-        items: 2, //3 items above 1024px browser width     
+        items: 2, //3 items above 1024px browser width
         itemsDesktop : [1024,1], //2 items between 1024px and 901px
         itemsDesktopSmall : [900,2], // betweem 900px and 601px
         itemsTablet: [600,1], //1 items between 600 and 0
@@ -123,12 +123,12 @@ jQuery(function($){
     jQuery(".prev2").click(function(){
         owl2.trigger('owl.prev');
     });
-	 
+
 
 	/* ----------------------------------------------------------- */
 	/*  7. MENU SMOOTH SCROLLING
-	/* ----------------------------------------------------------- */ 
-	
+	/* ----------------------------------------------------------- */
+
 	//MENU SCROLLING WITH ACTIVE ITEM SELECTED
 
 	// Cache selectors
@@ -148,7 +148,7 @@ jQuery(function($){
 	menuItems.click(function(e){
 	  var href = $(this).attr("href"),
 	      offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+15;
-	  jQuery('html, body').stop().animate({ 
+	  jQuery('html, body').stop().animate({
 	      scrollTop: offsetTop
 	  }, 900);
 	  e.preventDefault();
@@ -158,7 +158,7 @@ jQuery(function($){
 	jQuery(window).scroll(function(){
 	   // Get container scroll position
 	   var fromTop = $(this).scrollTop()+topMenuHeight;
-	   
+
 	   // Get id of current scroll item
 	   var cur = scrollItems.map(function(){
 	     if ($(this).offset().top < fromTop)
@@ -167,31 +167,30 @@ jQuery(function($){
 	   // Get the id of the current element
 	   cur = cur[cur.length-1];
 	   var id = cur && cur.length ? cur[0].id : "";
-	   
+
 	   if (lastId !== id) {
 	       lastId = id;
 	       // Set/remove active class
 	       menuItems
 	         .parent().removeClass("active")
 	         .end().filter("[href=#"+id+"]").parent().addClass("active");
-	   }           
+	   }
 	})
-    
+
 	/* ----------------------------------------------------------- */
-	/*  8. PRELOADER 
-	/* ----------------------------------------------------------- */ 
+	/*  8. PRELOADER
+	/* ----------------------------------------------------------- */
 
 	jQuery(window).load(function() { // makes sure the whole site is loaded
-      $('.progress').fadeOut(); // will first fade out the loading animation
-      $('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
-      $('body').delay(100).css({ 'overflow': 'visible' });
-     
+      //$('.progress').fadeOut(); // will first fade out the loading animation
+      //$('#preloader').delay(100).fadeOut('slow'); // will fade out the white DIV that covers the website.
+      //$('body').delay(100).css({ 'overflow': 'visible' });
     })
-	  
+
 	/* ----------------------------------------------------------- */
 	/* 9. CALL TO ABOUT
-	/* ----------------------------------------------------------- */ 
-	
+	/* ----------------------------------------------------------- */
+
 	jQuery(".call-to-about").click(function() {
     jQuery('html,body').animate({
         scrollTop: $("#about").offset().top},
@@ -200,7 +199,7 @@ jQuery(function($){
 
 	/* ----------------------------------------------------------- */
 	/* 10. BOTTOM TO UP
-	/* ----------------------------------------------------------- */ 
+	/* ----------------------------------------------------------- */
 
 	jQuery(".up-btn").click(function() {
     jQuery('html,body').animate({
@@ -210,13 +209,13 @@ jQuery(function($){
 
 	/* ----------------------------------------------------------- */
 	/* 11. PARALLAX HEADER
-	/* ----------------------------------------------------------- */ 
+	/* ----------------------------------------------------------- */
 
 	jQuery('.parallax').parallax();
 
 	/* ----------------------------------------------------------- */
 	/* 12. HIRE ME SCROLL
-	/* ----------------------------------------------------------- */ 
+	/* ----------------------------------------------------------- */
 
 	jQuery(".hire-me-btn").click(function(e) {
 		e.preventDefault();
@@ -226,10 +225,10 @@ jQuery(function($){
 	});
 
 
-		
-		
 
-	
+
+
+
 });
 
 
